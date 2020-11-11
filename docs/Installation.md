e5a835702f2f5db908a1b8a103c5328685471bed
Kristinn <kristinn@log2timeline.net>
2020-11-11 13:22:09 +0000

    Added end to end tests to Picatrix (#19)
    * Added end to end tests
    
    * Adding a new docker compose file.
    
    * Minor changes to the main readme

---
+++ docs/Installation.md
@@ -0,0 +1,94 @@
+# Installation
+
+Picatrix can be installed via two ways:
+
+1. docker
+2. pip inside a virtualenv
+
+Let's explore both methods:
+
+## Docker
+
+The easiest way to install picatrix is via docker. To use it, first
+[install docker](https://docs.docker.com/engine/install/).
+
+To run the docker container use:
+
+```shell
+$ git clone https://github.com/google/picatrix.git
+$ cd picatrix/docker
+$ sudo docker-compose -f docker-latest.yml --env-file config.env up -d
+```
+
+That will download the latest build and deploy the picatrix docker container.
+To be able to connect to picatrix in a jupyter shell, run:
+
+```shell
+$ CONTAINER_ID=`sudo docker container list | grep docker_picatrix | awk '{print $1}'`
+$ sudo docker logs $CONTAINER_ID
+...
+    To access the notebook, open this file in a browser:
+        file:///home/picatrix/.local/share/jupyter/runtime/nbserver-6-open.html
+    Or copy and paste one of these URLs:
+        http://....:8899/?token=...
+     or http://127.0.0.1:8899/?token=...
+...
+```
+
+Copy the URL that starts with `http://127.0.0.....` and paste it into a browser
+window and you should have a fully working copy of Jupyter running, with an
+active picatrix library.
+
+
+## Virtualenv
+
+To install picatrix using virtualenv you can use the following commands:
+
+```shell
+$ python3 -m venv picatrix_env
+$ source picatrix_env/bin/activate
+$ pip install picatrix
+```
+
+And then to start picatrix you can either run:
+
+```shell
+$ jupyter notebook
+```
+
+If you want to connect from a colab frontend, then you'll need to run these
+two commands as well:
+
+```shell
+$ pip install jupyter_http_over_ws
+$ jupyter serverextension enable --py jupyter_http_over_ws
+```
+
+And then to run the notebook:
+
+```shell
+$ jupyter notebook \
+  --NotebookApp.allow_origin='https://colab.research.google.com' \
+  --port=8888 \
+  --NotebookApp.port_retries=0
+```
+
+## Confirm Installation
+
+You can confirm the successful installation by creating a new notebook and type in:
+```
+%picatrixmagics
+```
+
+Picatrix library is already imported and initialized.
+
+## Connect To Colab
+
+In order to use the docker container for colab, you may need to change the URL
+that was provided from:
+
+`http://127.0.0.1:8899/?token=...` to `http://localhost:8899/?token=...`.
+
+Then select the arrow next to the `Connect` button, select `Connect to local
+runtime` and type in the URL with the token value into the `Backend URL`
+field and hit `CONNECT`.
---
+++ end_to_end_tests/interface.py
@@ -0,0 +1,90 @@
+# Copyright 2020 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Interface for end-to-end tests."""
+
+from typing import Callable
+from typing import Generator
+from typing import Text
+from typing import Tuple
+
+import unittest
+import inspect
+import collections
+import logging
+
+from IPython.terminal.interactiveshell import TerminalInteractiveShell
+from IPython.testing.globalipapp import start_ipython
+
+
+logger = logging.getLogger('picatrix.e2e_test')
+
+# Default values based on Docker config.
+TEST_DATA_DIR = '/usr/local/src/picatrix/end_to_end_tests/test_data'
+
+
+class BaseEndToEndTest:
+  """Base class for end to end tests.
+
+  Attributes:
+      assertions: Instance of unittest.TestCase
+  """
+  assertions: unittest.TestCase
+
+  NAME = 'name'
+
+  def __init__(self):
+    """Initialize the end-to-end test object."""
+    self.assertions = unittest.TestCase()
+    self._counter = collections.Counter()
+
+  def _get_test_methods(self) -> Generator[
+      Tuple[Text, Callable[[TerminalInteractiveShell], None]], None, None]:
+    """Inspect class and list all methods that matches the criteria.
+
+    Yields:
+        Function name and bound method.
+    """
+    for name, func in inspect.getmembers(self, predicate=inspect.ismethod):
+      if name.startswith('test_'):
+        yield name, func
+
+  def setup(self):
+    """Setup function that is run before any tests.
+
+    This is a good place to import any data that is needed.
+    """
+
+  def run_tests(self) -> collections.Counter:
+    """Run all test functions from the class.
+
+    Returns:
+        Counter of number of tests and errors.
+    """
+    ip = start_ipython()
+    ip.run_cell(raw_cell='from picatrix import notebook_init')
+    ip.run_cell(raw_cell='notebook_init.init()')
+
+    logger.info('*** %s ***', self.NAME)
+    for test_name, test_func in self._get_test_methods():
+      self._counter['tests'] += 1
+      logger.info('Running test: %s ...', test_name)
+      try:
+        test_func(ip)
+      except Exception:  # pylint: disable=broad-except
+        logger.error(
+            'Error while running test %s', self.NAME, exc_info=True)
+        self._counter['errors'] += 1
+        continue
+      logger.info('%s [OK]', test_name)
+    return self._counter
