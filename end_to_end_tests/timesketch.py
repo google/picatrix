3452aadb0dcd146a2ec57501ee31a4fbfeb761a5
Kristinn Gudjonsson <kristinn@log2timeline.net>
2020-11-13 13:14:44 +0000

    update

--- end_to_end_tests/timesketch.py
+++ end_to_end_tests/timesketch.py
@@ -15,8 +15,6 @@
 
 from IPython.terminal.interactiveshell import TerminalInteractiveShell
 
-from timesketch_api_client import client
-
 from . import interface
 from . import manager
 
@@ -26,40 +24,28 @@ class TimesketchTest(interface.BaseEndToEndTest):
 
   NAME = 'timesketch_test'
 
-  _client: client.TimesketchApi = None
-
-  def _get_client(self) -> client.TimesketchApi:
-    """Returns a TimesketchApi client object."""
-    if self._client:
-      return self._client
-
-    self._client = client.TimesketchApi(
-        host_uri='https://demo.timesketch.org',
-        username='demo',
-        password='demo',
-        verify=True,
-        auth_mode='userpass')
-    return self._client
-
-  def _setup(self, ip, sketch_id=0):
-    """Setup the test."""
-    client = self._get_client()
+  def _setup_client(self, ip):
+    """Setup the TimesketchAPI object into the IPython session."""
+    ip.run_cell(raw_cell='from timesketch_api_client import client')
+    _ = ip.run_cell(raw_cell=(
+        '_client = client.TimesketchApi(\n'
+        '    host_uri="https://demo.timesketch.org",\n'
+        '    username="demo",\n'
+        '    password="demo",\n'
+        '    verify=True,\n'
+        '    auth_mode="userpass")'))
     _ = ip.run_cell(raw_cell=(
         'from picatrix.lib import state\n'
         'state_obj = state.state()\n'
-        'state_obj.add_to_cache(\'timesketch_client\', client)\n'
+        'state_obj.add_to_cache(\'timesketch_client\', _client)\n'
     ))
-    if sketch_id:
-      _ = ip.run_cell(raw_cell=(
-          f'sketch = client.get_sketch({sketch_id})\n'
-          f'state_obj.add_to_cache(\'timesketch_sketch\', sketch)\n'))
 
   def test_get_sketch(self, ip: TerminalInteractiveShell):
     """Test fetching a sketch."""
-    self._setup(ip)
+    self._setup_client(ip)
     sketch = ip.run_cell('client.get_sketch(6)')
-    self.assertions.assertEquals(sketch.id, 6)
-    self.assertions.assertEquals(sketch.name, 'Szechuan Sauce - Challenge')
+    self.assertions.assertEqual(sketch.id, 6)
+    self.assertions.assertEqual(sketch.name, 'Szechuan Sauce - Challenge')
 
 
 manager.EndToEndTestManager.register_test(TimesketchTest)
